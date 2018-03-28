bf2709ebcdddcd6f8105697223f26487fc4efc5d
Jassirlink <jassirlink@gmail.com>
2018-03-28 23:21:45 +0530

    pls add the wrker

---
+++ work.bat
@@ -0,0 +1,14 @@
+what a heavy worker
+ 
+
+        get off the fucking job .........
+        
+         and enjoy the life 
+          
+          thats all my dudzzz
+           
+
+        go and enjoy the blooody life
+       
+
+          ho hohohohho
